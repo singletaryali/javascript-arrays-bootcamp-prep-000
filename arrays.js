d0b1e75aedd59d33e0a0d64ea3915672fafb736b
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-08-30 01:20:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,5 +4,6 @@ function addElementToBeginningOfArray (array, element) {
    return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  
+  array.unshift('element')
+  return array
 }
