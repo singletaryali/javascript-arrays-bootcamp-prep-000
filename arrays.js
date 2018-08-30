cf00710ef14e69fbff34f4120a5c20eb9862fbda
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-08-30 02:29:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,6 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
 }
 function removeElementFromBeginningOfArray (array) {
   return array.slice(1);
-  
 }
 function destructivelyRemoveElementFromEndOfArray (array) {
   return array.pop()
