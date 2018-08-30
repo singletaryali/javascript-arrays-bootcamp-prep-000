b3f845d81aafece2850a00f7f1d45a4205ef55d3
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-08-30 00:45:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
 function addElementToBeginningOfArray ( ) {
   return 
 }
-function (array, element)
-var array = foo
-var element = '1' 
+function (array, element){
+var array = foo;
+var element = '1' ;
+return (array, element);
+}
